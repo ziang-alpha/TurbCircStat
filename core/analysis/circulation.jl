f9baec71afbf57efa3b260079a132837621ad2ea
zzalpha-phys <zzalpha2024@163.com>
2026-01-11 17:53:56 +0800

    modify file structure

---
+++ core/analysis/circulation.jl
@@ -0,0 +1,32 @@
+module Circulation
+
+"""
+    Build a loop by combine a sequence of rectangular loops. 
+    'heights', 'widths', 'startxs', 'startys' specify the height, width
+    and the location left-bottom corner of each rectangle.
+"""
+function rectBuildLoop(grid, heights, widths, startxs, startys)
+	hs = zeros(Bool, grid.nx, grid.ny)
+	for (h, w, x, y) in zip(heights, widths, startxs, startys)
+		hs .= hs .|| rectangle(h, w, x, y, grid)
+	end
+	hs = device_array(grid)(hs)
+	return grid.rfftplan * hs
+end
+
+rectangle(height, width, startx, starty, grid) = @. (0 < (grid.x - startx) < width) && (0 < (grid.y-starty)' < height)
+
+
+
+
+"""
+    Compute the velocity circulation
+    using the convolution of the vorcity field 'ζ' and the loop Heaviside 'hs'.
+    'ζh' and 'hsh' are theri Fourier transforms respectively.
+"""
+function getΓ(ζh, hsh, grid)
+	Γh = device_array(grid)(ζh .* hsh) * (grid.dx * grid.dy)
+	return grid.rfftplan \ Γh
+end
+
+end
---
+++ paper-2026-2D-abs-eq/run_sim.jl
@@ -0,0 +1,61 @@
+using GeophysicalFlows, CUDA, Random, HDF5, ProgressMeter
+
+include("params_sim.jl")
+
+# Get the forcing spectrum
+forcing_spectrum = begin
+	forcing_wavenumber = kf * 2π/L  # the forcing wavenumber, `k_f`, for a spectrum that is a ring in wavenumber space
+	forcing_bandwidth = 1.5 * 2π/L  # the width of the forcing spectrum, `δ_f`
+
+	grid = TwoDGrid(dev; nx = ngrid, Lx = L)
+
+	K = @. sqrt(grid.Krsq)             # a 2D array with the total wavenumber
+
+	forcing_spectrum = @. exp(-(K - forcing_wavenumber)^2 / (2 * forcing_bandwidth^2))
+	CUDA.@allowscalar forcing_spectrum[grid.Krsq .== 0] .= 0 # ensure forcing has zero domain-average
+
+	ε0 = FourierFlows.parsevalsum(forcing_spectrum .* grid.invKrsq / 2, grid) / (grid.Lx * grid.Ly)
+	forcing_spectrum .*= ε/ε0        # normalize forcing to inject energy at rate ε	
+	forcing_spectrum
+end
+
+# Functions describing the random forcing
+function calcF!(Fh, sol, t, clock, vars, params, grid)
+	randn!(Fh)
+	@. Fh *= sqrt(forcing_spectrum) / sqrt(clock.dt)
+	return nothing
+end
+
+# Instantiate the TwoDNavierStokes problem
+prob = TwoDNavierStokes.Problem(dev;
+	nx = ngrid,
+	Lx = L,
+	ν,
+	dt = ε^(-1/3) * kf^(-2/3) * CFL_forcing,
+	stepper = "ETDRK4",
+	calcF = calcF!,
+	stochastic = true,
+)
+
+# Initialize out put file & get the initial value.
+if isfile(output_path)
+	sol = h5open(output_path, "r") do f
+		frames = [parse(Int, k) for k in keys(f)]
+		read(f, "$(maximum(frames))")
+	end
+	ζ₀ = prob.grid.rfftplan\device_array(dev)(sol)
+	set_ζ!(prob, ζ₀)
+	mv(output_path, output_path*".old", force = true)
+else
+	set_ζ!(prob, device_array(dev)(zeros(ngrid, ngrid)))
+end
+
+
+fid = h5open(output_path, "w")
+
+@showprogress for nframe in 1:ndata
+	stepforward!(prob, nstep)
+	write_dataset(fid, "$(nframe)", Array(prob.sol))
+end
+
+close(fid)
