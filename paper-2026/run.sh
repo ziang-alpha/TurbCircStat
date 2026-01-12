336b1ce4292a0a1acfd97ceacc5e4fced1b3e52f
ziang-alpha <zzalpha2024@163.com>
2026-01-12 23:26:25 +0800

    run.sh modified

--- paper-2026/run.sh
+++ paper-2026/run.sh
@@ -1,1 +1,3 @@
-julia --project=../ simulation.jl $1 $2 $3 $4
+#!/bin/bash 
+DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
+julia --project="${DIR}" "${DIR}/paper-2026/simulation.jl" $1 $2 $3 $4
